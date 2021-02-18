55dd84656ae3556944ce2fcebaed9e2a49dbc18f
Phillip Alexander <phillip.alexander@galvanize.com>
2021-02-18 13:00:48 +0000

    (init)

---
+++ mInteger/server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const db = require("../database-mysql");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/../client/dist"));
+
+app.get("/api/transactions", (req, res) => {
+  //TODO - your code here!
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
